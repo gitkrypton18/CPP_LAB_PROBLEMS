4e76e100e3785c42c8386c5e952d069d5b4f4b12
KALPIT NAGAR <kalpitnagar312@gmail.com>
2024-12-14 18:15:29 +0530

    .

--- LAB 7/84_f_pointerBookInfo.cpp
+++ LAB 7/84_f_pointerBookInfo.cpp
@@ -9,26 +9,28 @@ struct BookInfo
     string AuthorName;
 };
 
-void bookinfoRead(BookInfo *Book)
+void bookinfoRead(BookInfo *Book) // taking the address of the variable data type in this function
 {
     cout << "Enter the book Name :" << endl;
-    getline(cin, *Book.Name);
+    getline(cin, Book->Name); // -> operator can be used to access the elements of the structure
     cout << "Enter the Author Name :" << endl;
-    getline(cin, *Book.AuthorName);
+    getline(cin, Book->AuthorName);
     cout << "Enter the Publication Year :" << endl;
-    cin > Book.PubYear;
+    cin >> Book->PubYear;
 }
 
-void bookinfoDisplay(BookInfo *Book)
+void bookinfoDisplay(BookInfo *Book) // taking the address of the variable data type in this function
 {
-    cout << "The book Name is : " << Book.Name << endl;
-    cout << "The Author Name is : " << Book.AuthorName << endl;
-    cout << "The Publication Year is : " << Book.PubYear << endl;
+    cout << "The book Name is : " << Book->Name << endl;
+    cout << "The Author Name is : " << Book->AuthorName << endl;
+    cout << "The Publication Year is : " << Book->PubYear << endl;
 }
+
 int main()
 {
-
     BookInfo Book;
+
+    // Reading and displaying book information using pointers
     bookinfoRead(&Book);
     bookinfoDisplay(&Book);
 
